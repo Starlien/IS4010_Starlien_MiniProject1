b744661d3dee891ae5c50056f73c6847a57c86c2
Starlien <Starlien@mail.uc.edu>
2021-02-05 15:48:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.until.Scanner;
+
+/*
+Mini Project 1
+Group #_: Evan Starling, YourPartnerName1, YourPartnerName2, YourPartnerName3 (if you have 4 members)
+Date: (whatever date you turn it in)
+*/
+class Main {
+  public static void main(String[] args) {
+    java.util.Scanner s = new Scanner(System.in);
+    int userNum;
+    System.out.println("Welcome to the guessing game!");
+    System.out.println("Please enter any positive whole number:");
+    userNum = s.nextInt();
+  }
+}
